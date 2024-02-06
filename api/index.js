10ecaf59229afb376a1970e7a17133bc04d6dcfd
vithanh173 <jhjmhn@gmail.com>
2024-02-06 13:11:28 +0700

    connect to database and create use model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error", error);
+  });
