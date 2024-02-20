0624183e6eaffe249cf9c6cb15756e5ea14fedcc
vithanh173 <jhjmhn@gmail.com>
2024-02-20 16:17:48 +0700

    complete get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 
 import { verifyToken } from "../libs/utils.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getPosts } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createPost);
+router.get("/getposts", getPosts);
 
 export default router;
