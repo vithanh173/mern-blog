ceba138b7d1cd52cd405620a7b650f6f32e02282
vithanh173 <jhjmhn@gmail.com>
2024-02-22 13:45:47 +0700

    complete like comment func

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import moment from "moment";
+import { FaThumbsUp } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-const Comment = ({ comment }) => {
-  const [user, setUser] = useState({});
+const Comment = ({ comment, onLike }) => {
+  const [userComment, setUserComment] = useState({});
+  const { user } = useSelector((state) => state.user);
 
   useEffect(() => {
     const getUser = async () => {
@@ -10,7 +13,7 @@ const Comment = ({ comment }) => {
         const res = await fetch(`/api/user/${comment.userId}`);
         const data = await res.json();
         if (res.ok) {
-          setUser(data);
+          setUserComment(data);
         }
       } catch (error) {
         console.log(error.message);
@@ -22,16 +25,35 @@ const Comment = ({ comment }) => {
   return (
     <div className="flex p-4 border-b dark:border-gray-600 text-sm">
       <div className="flex-shrink-0 mr-3">
-        <img src={user.image} alt={user.username} className="w-10 h-10 rounded-full bg-gray-200" />
+        <img
+          src={userComment.image}
+          alt={userComment.username}
+          className="w-10 h-10 rounded-full bg-gray-200"
+        />
       </div>
       <div className="flex-1">
         <div className="flex items-center mb-1">
           <span className="font-bold mr-1 text-xs truncate">
-            {user ? `@${user.username}` : "Anomyous User"}
+            {userComment ? `@${userComment.username}` : "Anomyous User"}
           </span>
           <span className="text-gray-500 text-xs">{moment(comment.createdAt).fromNow()}</span>
         </div>
         <p className="text-gray-500 pb-2">{comment.content}</p>
+        <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+          <button
+            type="button"
+            className={`text-gray-400 hover:text-blue-500 ${
+              user.currentUser && comment.likes.includes(user.currentUser._id) && "!text-blue-500"
+            }`}
+            onClick={() => onLike(comment._id)}
+          >
+            <FaThumbsUp className="text-sm" />
+          </button>
+          <p>
+            {comment.numberOfLikes > 0 &&
+              comment.numberOfLikes + " " + (comment.numberOfLikes === 1 ? "like" : "likes")}
+          </p>
+        </div>
       </div>
     </div>
   );
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Alert, Button, Textarea } from "flowbite-react";
 import Comment from "./Comment";
 
@@ -9,8 +9,7 @@ const CommentSection = ({ postId }) => {
   const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
   const [error, setError] = useState(null);
-
-  console.log(comments);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getComments = async () => {
@@ -48,6 +47,34 @@ const CommentSection = ({ postId }) => {
       }
     } catch (error) {
       setError(error.message);
+    }
+  };
+
+  const handleLike = async (commentId) => {
+    try {
+      if (!user.currentUser) {
+        navigate("/sign-in");
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: "PUT",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
     }
   };
 
@@ -106,7 +133,7 @@ const CommentSection = ({ postId }) => {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} />
+            <Comment comment={comment} onLike={handleLike} />
           ))}
         </>
       )}
