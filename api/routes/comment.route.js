30fe50a3362850c92a70d28d5f1af233250a18de
vithanh173 <jhjmhn@gmail.com>
2024-02-22 14:39:49 +0700

    complete edit comment func

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,6 +1,11 @@
 import express from "express";
 
-import { createComment, getPostComments, likeComment } from "../controllers/comment.controller.js";
+import {
+  createComment,
+  editComment,
+  getPostComments,
+  likeComment,
+} from "../controllers/comment.controller.js";
 import { verifyToken } from "../libs/utils.js";
 
 const router = express.Router();
@@ -8,5 +13,6 @@ const router = express.Router();
 router.post("/create", verifyToken, createComment);
 router.get("/getPostComments/:postId", getPostComments);
 router.put("/likeComment/:commentId", verifyToken, likeComment);
+router.put("/editComment/:commentId", verifyToken, editComment);
 
 export default router;
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -78,6 +78,12 @@ const CommentSection = ({ postId }) => {
     }
   };
 
+  const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map((c) => (c._id === comment._id ? { ...c, content: editedContent } : c))
+    );
+  };
+
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {user.currentUser ? (
@@ -133,7 +139,7 @@ const CommentSection = ({ postId }) => {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment comment={comment} onLike={handleLike} />
+            <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit} />
           ))}
         </>
       )}
